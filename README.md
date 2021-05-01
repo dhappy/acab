5e96a3fdc8f0cdb82781fb4bd864aa5228be3fa3
Will Holcomb <dys@dhappy.org>
2021-04-30 23:39:45 -0400

    fixin' published version 🎭

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ACAB
 
-ACAB is, at it's heart a roving car. It always carries at least two people. There are different roles the two people can perform:
+ACAB is, at it's heart, a roving car. It always carries at least two people. There are different roles the two people can perform:
 
 * As Driver and Dispatcher handling incoming calls.
 * As Driver and a Clerk who provides access to an in-car inventory.
@@ -12,3 +12,24 @@ ACAB is, at it's heart a roving car. It always carries at least two people. Ther
   * The Mediator responsible for interfacing with anyone external to the team.
   * Comms responsible for tools and interfacing with the organization at large.
 
+## Dispatch
+
+Partial contracts are distributed P2P.
+
+The dispatch interface is for handling reviewing potential contracts.
+
+The person placing the call creates a chat which potential drivers join.
+
+Cars bid on calls.
+
+## Store
+
+The car carries an inventory and sells items. 
+
+## Gym
+
+Fighters are compensated for time spent training.
+
+## Creation
+
+People define themselves by what they are not. They are not proactively deciding, but instead choosing actions based on avoidance.
---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import './App.css'
+
+export default () => {
+  return (
+    <div id="App">
+      <p><acronym title='All Cops Are Bastards'>ACAB</acronym> wants to out-police the police.</p>
+
+      <p>Ideally, a car (or combination of cars) carries a team of five people:</p>
+      
+      <ul>
+        <li>The Driver responsible for the vehicle.</li>
+        <li>The Cameraperson responsible for livestreaming everything to the cloud.</li>
+        <li>The Muscle responsible for any physical tasks.</li>
+        <li>The Liason responsible for interfacing with anyone external to the team.</li>
+        <li>Comms responsible for tools and interfacing with the organization at large.</li>
+      </ul>
+
+      <p>Because of the pandemic, these five roles can be filled by two people. At the least, the Liason and the Cameraperson need to be separate people. The Cameraperson should be solely concerned with getting a recording of the events uploaded to the cloud.</p>
+
+      <p>Like with 911, your first contact is with a dispatcher. They handle finding a team to respond.</p>
+
+      <p>Patrols report their position to dispatch. Participants are paid a fixed wage for time on patrol or training.</p>
+
+      <p>I would like all counter-police activities to be saved in the cloud and have some system of governance for its publicity. The simplest way to do it is to make all activities public, but that could dissuade people with private situations from using the service.</p>
+
+      <p>I would like to see the widespread use of non-lethal rounds. I would like a general rule that firearms will never be brandished first by our agents.</p>
+
+      <p>One initial focus is traffic stops. As someone is getting stopped, they tell their smart phone and active patrols are notified.</p>
+
+      <p>The vehicle pulls in front of the stop and the Liason followed by the Cameraperson approach the scene.</p>
+
+      <p>One common police tactic is swarming and overpowering any opposition through sheer numbers. I want the responding team to be able to call in more teams if this happens.</p>
+
+      <ul>
+        <li><button>Patrols In Your Area</button></li>
+      </ul>
+
+      <p>When the pair is in the car without a call they operate as couriers for both ridesharers and goods.</p>
+
+      <p>If a ride is interrupted by a call, the dispatcher handles rerouting anything in the car.</p>
+    </div>
+  )
+}
